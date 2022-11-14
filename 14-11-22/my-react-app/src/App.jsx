6419b4d467abbe9e27f281e2a9cf08a8502bf586
DarXik <73177923+DarXik@users.noreply.github.com>
2022-11-14 16:59:04 +0100

    react

--- 14-11-22/my-react-app/src/App.jsx
+++ 14-11-22/my-react-app/src/App.jsx
@@ -2,13 +2,24 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Header from "./components/Header";
+import ListItem from "./components/ListItem";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div>
-    <Header></Header>
-  </div>;
+  return (
+    <div>
+      <Header/>
+      <ul>
+        <ListItem text="WBA" />
+        <ListItem text="PVA" />
+        <ListItem text="APS" />
+        <ListItem text="TEV" />
+        <ListItem text="MAT" />
+       
+      </ul>
+    </div>
+  );
 }
 
 export default App;
---
+++ 14-11-22/my-react-app/src/components/ListItem.jsx
@@ -0,0 +1,4 @@
+function ListItem(param) {
+  return <li className="list-item">{param.text}</li>;
+}
+export default ListItem;
