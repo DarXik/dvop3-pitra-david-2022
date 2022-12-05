d5038f04aea6b17c43ba00adfe99998998e16ea8
DarXik <73177923+DarXik@users.noreply.github.com>
2022-12-05 16:58:42 +0100

    tailwind

---
+++ 5-12-22/my-react-app/src/components/Card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const CardBlue = () => {
+  return <div>CardBlue</div>;
+};
+
+const Card = (props) => {
+  console.log("Card props: ", props);
+  const { data } = props;
+  return (
+    <div>
+      Card
+      <p>Data z App.jsx: {props.data}</p>
+      <p>Data z App.jsx: {data}</p>
+    </div>
+  );
+};
+
+export default Card;
