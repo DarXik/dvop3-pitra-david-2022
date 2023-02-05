842fb0a73f46fcbd05d079f63b0d3f31c4c4a9c5
DarXik <davca749@gmail.com>
2023-02-05 14:21:35 +0100

    random color

--- individual-work/react-rest-api-1/src/App.jsx
+++ individual-work/react-rest-api-1/src/App.jsx
@@ -1,56 +1,60 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
-import Todo from "../components/todo.jsx";
+import Todo from "./components/todo.jsx";
+import UseGenerateRandomColor from "./random";
+import TailwindColor from "@videsk/tailwind-random-color";
 
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  //   const { color, generateColor } = UseGenerateRandomColor();
+  const fetchData = async () => {
+    setLoading(true);
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
+    const responseJson = await response.json();
+    setData(responseJson);
+    // console.log(responseJson);
+    setLoading(false);
+  };
 
-    const fetchData = async () => {
-        setLoading(true);
-        const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
-        const responseJson = await response.json();
-        setData(responseJson);
-        console.log(responseJson);
-        setLoading(false);
-    };
+  //   var barvicky = []
+  //   data.map(()=>{generateColor(); barvicky.push(color)})
 
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-    return (
-        // <div>
-        //   <h1>React App</h1>
-        //   {loading ? (
-        //     <p>Loading...</p>
-        //   ) : (
-        //     <div>
-        //       {data ? (
-        //         <div>
-        //           {data.map((item) => (
-        //             <div key={item.id}>
-        //               <p>{item.title}</p>
-        //             </div>
-        //           ))}
-        //         </div>
-        //       ) : (
-        //         <p>No data</p>
-        //       )}
-        //     </div>
-        //   )}
-        // </div>
-        <>
-            <div className="grid lg:grid-cols-3 grid-cols-1 gap-y-[2em] gap-x-[2em] mx-[25%] mt-6 lg:text-lg">
-                <Todo myClass="border-4 border-indigo-400 p-2" mySpan="font-bold" title="Udělat večeři" userid="15"
-                      id="1"></Todo>
-                <Todo myClass="border-4 border-amber-800 p-2" mySpan="font-bold" title="Nakrmit kocour" userid="1"
-                      id="3"></Todo>
-                <Todo myClass="border-4 border-lime-500 p-2" mySpan="font-bold" title="Přečíst knihu" userid="15"
-                      id="2"></Todo>
-            </div>
-        </>
-    );
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const [color, setColor] = useState(new TailwindColor().pick());
+  console.log(color);
+  return (
+    <>
+      <div>
+        {/* <button onClick={()=>console.log(barvicky)}>AHOJAOHO</button> */}
+        <h1 className="underline text-3xl text-center mt-3 mb-4">TODO</h1>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <div>
+            {data ? (
+              <div className="grid lg:grid-cols-3 grid-cols-1 gap-y-[2em] gap-x-[2em] mx-[10%] mt-6 mb-6">
+                {data.map((item) => (
+                  <Todo
+                    color={color    }
+                    key={item.id}
+                    title={item.title}
+                    userid={item.userId}
+                    id={item.id}
+                  ></Todo>
+                ))}
+              </div>
+            ) : (
+              <p>No data</p>
+            )}
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default App;
+// console.log(new TailwindColor().pick())
---
+++ individual-work/react-rest-api-1/src/components/todo.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import TailwindColor from "@videsk/tailwind-random-color";
+
+const Todo = (props) => {
+  
+ 
+  return (
+    <div className={props.color + " border-4 lg:text-lg p-2 "}>
+      <h1>
+        <span className={props.mySpan}>Title:</span> {props.title}
+      </h1>
+      <h2>
+        <span className={props.mySpan}>UserID:</span> {props.userid}
+      </h2>
+      <p>
+        <span className={props.mySpan}>ID:</span> {props.id}
+      </p>
+    </div>
+  );
+};
+
+export default Todo;
